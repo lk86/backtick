6c21c404f34afb70fe32bdb043d71e2bbc528472
BuildTools <git@lhk.me>
2016-01-22 03:10:57 -0500

    Added help text for each command

--- iicmd.sh
+++ iicmd.sh
@@ -25,10 +25,38 @@ qdb() {
     head -1 $file
 }
 
+help() {
+    case "$1" in
+        man)
+            txt='Description: Display help text. Usage: `man <command>` - prints help text for <command>. | `man` - prints command list and github link.' ;;
+        bc)
+            txt='Description: Perform calculations with GNU bc, the basic calculator. Usage: `bc <bc expression>` - prints one line of bc -lsq output, if calculation completes in <30 seconds' ;;
+        qdb)
+            txt='Description: Adds quotes to the quote database. Usage: `qdb <start regex> <end regex>` - adds messages sent between a message matching <start regex> and one matching <end message> inclusive to qdb' ;;
+        echo)
+            txt='Description: Echoes back its input. Usage: `echo <string>` - prints <string>, sanitized to not trigger averybot, lurker, or ii' ;;
+        grep)
+            txt='Description: Searches for qdb entry matching a given pattern. Usage: `qdb <pattern>` - prints the last 10 lines of the first matching qdb entry found' ;;
+        fortune)
+            txt='Description: Fortune cookie database interface. Usage: `fortune` - prints a random fortune from the database. | `fortune <pattern>` - prints the first 10 lines of matching fortune cookies' ;;
+        ping)
+            txt='Description: Test function to verify bot functionality. Usage: `ping` - replies "pong!"' ;;
+        w)
+            txt='Description: Queries en.wikipedia.org for a page, and returns a summary of the topic. Usage: `w <page>` - prints either the first 4 sentences or first graph (whichever is shorter) of wiki entry <page>' ;;
+       *)
+            txt='Command not found. This functionality is either not availible or not stable' ;;
+esac
+
+printf -- "%s\n" "${txt}"
+}
+
 case "$cmd" in
     man)
-        printf -- "Commands: %s | Source: https://github.com/lk86/backtick\n" "${commands[*]}"
-        ;;
+        if [[ -z "${extra}" ]]; then
+            printf -- "Commands: %s | Source: https://github.com/lk86/backtick\n" "${commands[*]}"
+        else
+            help ${extra#/}
+        fi ;;
     bc)
         export BC_LINE_LENGTH=0
         tail <<< "$nick: $(timeout 30 bc -lsq <<< "$extra")"
@@ -45,15 +73,13 @@ case "$cmd" in
             tail "$file"
         else
             echo "QDB entry not found"
-        fi
-        ;;
+        fi ;;
     fortune)
         if [[ -n "$extra" ]]; then
             tail <<< "$(fortune -sea -m "${extra#/}")"
         else
             printf -- "%s\n" "$(fortune -sea)"
-        fi
-        ;;
+        fi ;;
     ping)
         printf -- "%s: pong!\n" "$nick"
         ;;
